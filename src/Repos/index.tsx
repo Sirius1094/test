768faf7330dc86b0aac873d5497582c4c48b9c65
Sirius1094 <liuteng@njust.edu.cn>
2024-08-03 23:36:06 -0700

    update Repo file

--- src/Repos/index.tsx
+++ src/Repos/index.tsx
@@ -1,13 +1,14 @@
 export default function Repos() {
     return (
-        <div id="wd-labs">
+        <div id="wd-repos">
             <h1>Teng Liu - WebDev Summer 2 2024</h1>
             <h2>Code Repositories</h2>
             <ul id="wd-repo-navigation">
                 <li><a id="wd-a1" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a1">Assignment 1 - Github</a></li>
                 <li><a id="wd-a2" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a2">Assignment 2 - Github</a></li>
                 <li><a id="wd-a3" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a3">Assignment 3 - Github</a></li>
-                <li><a id="wd-a3" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a4">Assignment 4 - Github</a></li>
+                <li><a id="wd-a4" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a4">Assignment 4 - Github</a></li>
+                <li><a id="wd-a5" target="_blank" href="https://github.com/Sirius1094/kanbas-react-web-app/tree/a5">Assignment 5 - Github</a></li>
                 <br />
             </ul>
             <a id="wd-back-to-landing" href="/">Back to Landing Page</a>
